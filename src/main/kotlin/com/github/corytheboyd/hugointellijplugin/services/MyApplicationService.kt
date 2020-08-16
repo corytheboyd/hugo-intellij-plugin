0244e5d30154f0ef5f5c6db994fe5e7487ff41f9
GitHub Action <action@github.com>
2020-08-16 19:50:29 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/corytheboyd/hugointellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.corytheboyd.hugointellijplugin.services
+
+import com.github.corytheboyd.hugointellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/corytheboyd/hugointellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/corytheboyd/hugointellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.corytheboyd.hugointellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.corytheboyd.hugointellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
